398d6170b3649d2a36e44ae01b3af4f75196c1d9
Ennosto <ennosto_me@mail.ru>
2024-09-10 17:12:45 +0300

    Update module_5_4.py
    По индексу

--- module_5_4.py
+++ module_5_4.py
@@ -1,7 +1,7 @@
 class House:
     houses_history = []
     def __new__(cls, *args, **kwargs):
-        cls.houses_history.append(args)
+        cls.houses_history.append(args[0])
         return object.__new__(cls)
 
 
